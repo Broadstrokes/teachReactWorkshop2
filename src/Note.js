84112cca8d1b78d8d8c081ced7a012955f563800
Dan Sajjad <dan.sajjad@gmail.com>
2018-05-26 19:53:17 -0700

    Add renderForm method; edit functionality

--- src/Note.js
+++ src/Note.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
+
 
 class Note extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            editing: false
+        }
+
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.save = this.save.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderNote = this.renderNote.bind(this)
     }
 
-    edit() { alert('editing note') }
+    edit() { alert('editing note'); this.setState({editing: true}) }
     remove() { alert('removing note') }
+    save() { alert('saving note') }
 
-    render() {
+    renderForm() {
+        return (
+            <div className='note'>
+                <form>
+                    <textarea />
+                    <button onClick={this.save} id='save'><FaFloppyO /></button>
+                </form>
+            </div>
+        )
+    }
+
+    renderNote() {
         return (
             <div className='note'>
                 <p>Learn React</p>
@@ -22,6 +43,10 @@ class Note extends Component {
                 </span>
             </div> 
         )
+    }
+
+    render() {
+        return this.state.editing ? this.renderForm() : this.renderNote()
     }
 }
 

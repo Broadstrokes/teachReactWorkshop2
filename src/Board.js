45e7b80135f6e39d77ae286e865f152350bb4057
Dan Sajjad <dan.sajjad@gmail.com>
2018-05-26 21:21:48 -0700

    Make board component stateful

--- src/Board.js
+++ src/Board.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react'
 import Note from './Note'
 
 class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: [
+                {id:1, note: 'Call Jimmy'},
+                {id:12, note: 'Pickup laundry'},
+            ]
+        }
+        this.eachNote = this.eachNote.bind(this)
+    }
+
+    eachNote(note, i) {
+        return (
+            <Note key={i} index={i}>{note.note}</Note>
+        )
+    }
+
     render() {
         return (
             <div className='board'>
-                <Note />
+                {this.state.notes.map(this.eachNote)}
             </div>
         )
     }
